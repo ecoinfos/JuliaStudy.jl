694c7ec81aee7882c569e33d182ec0eb28d293b3
Documenter.jl <documenter@juliadocs.github.io>
2023-07-29 09:48:24 +0000

    build based on 11d0ff1

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = JuliaStudy","category":"page"},{"location":"#JuliaStudy","page":"Home","title":"JuliaStudy","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for JuliaStudy.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [JuliaStudy]","category":"page"}]
+}

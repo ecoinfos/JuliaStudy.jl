3331255ff1cb224748a4279d1cf6b13a708ad377
Norel <norel.evoagile@gmail.com>
2023-11-09 20:20:41 +0900

    comment out for codes with multithread env for Julia 1.8 compatibility.

--- test/JuliaPatterns/ch11/tests_singleton_pattern.jl
+++ test/JuliaPatterns/ch11/tests_singleton_pattern.jl
@@ -2,10 +2,11 @@ using Test
 import JuliaStudy.JuliaPatterns.SingletonPattern as SP
 
 @testset "Singleton pattern " begin
-  Threads.nthreads()
-  Threads.threadpool()
-
-  SP.test_multithreading()
-
-  SP.test_multithreading2()
+  # Need to start with JULIA_NUM_THREADS=4 julia --project=.
+  # Threads.nthreads()
+  # Threads.threadpool()
+  #
+  # SP.test_multithreading()
+  #
+  # SP.test_multithreading2()
 end
